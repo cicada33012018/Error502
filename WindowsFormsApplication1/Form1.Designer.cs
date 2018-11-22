ab5a26bf6bed277b3233392c1d26c1bb8b9ab525
Taha <taha24888@gmail.com>
2018-11-22 21:43:21 +0500

    abc

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
@@ -42,22 +41,12 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Controls.Add(this.listBox1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(866, 581);
             this.panel1.TabIndex = 1;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(303, 38);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(152, 303);
-            this.listBox1.TabIndex = 3;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button2
             // 
@@ -122,7 +111,6 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -18,16 +18,14 @@ namespace WindowsFormsApplication1
 
     public partial class Form1 : Form
     {
-        //string selectedFolderPath;
-       // string selectedFilePath;
         List<FileDetail> Files;
         public static string zee = "";
+        public static int counter = 0;
 
         public Form1()
         {
             InitializeComponent();
             Files = new List<FileDetail>();
-            listBox1.Visible = false;
             button2.Visible = false;
 
         }
@@ -41,10 +39,9 @@ namespace WindowsFormsApplication1
                     {
                         button1.Visible = false;
                         button2.Visible = false;
-                        listBox1.Visible = true;
                     }
             Files = getFiles(selectedFolderPath);
-
+           
 
 
         }
@@ -90,13 +87,32 @@ namespace WindowsFormsApplication1
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
         }
-
+        private static int CalculateComplexity(string Path)
+        {
+            string line;
+            int complexity = 0;
+            System.IO.StreamReader str = new System.IO.StreamReader(@"" + Path);
+            while ((line = str.ReadLine()) != null)
+            {
+                if (line.Contains("void") || line.Contains("int") || line.Contains("float") || line.Contains("string") || line.Contains("char") && line.Contains("("))
+                { complexity++; }
+                if (line.Contains("if") || line.Contains("else") || line.Contains("case") || line.Contains("default"))
+                    complexity++;
+                if (line.Contains("for") || line.Contains("while") || line.Contains(" do ") || line.Contains("break") || line.Contains("continue"))
+                    complexity++;
+                if (line.Contains("&&") || line.Contains("||") || line.Contains("?"))
+                    complexity++;
+                if (line.Contains("catch") || line.Contains("finally") || line.Contains("throw") || line.Contains("throws"))
+                    complexity++;
+            }
+            return complexity;
+        }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             var FileName = dataGridView1.SelectedCells[0].Value;
             var FileDirectory = dataGridView1.SelectedCells[1].Value;
             string FilePath = FileDirectory + "/" + FileName;
-            MessageBox.Show(FilePath);
+            MessageBox.Show(""+CalculateComplexity(FilePath));
         }
     }
 }
